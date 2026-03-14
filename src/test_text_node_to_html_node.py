2af8426be8c87a1aaa5a37469db946c8ce7b83d6
Algacyr Junior <alsdmelo@protonmail.com>
2026-03-14 19:08:42 -0300

    Implement text_node_to_html_node function

---
+++ src/test_text_node_to_html_node.py
@@ -0,0 +1,12 @@
+import unittest
+
+from text_node_to_html_node import text_node_to_html_node
+from textnode import TextNode, TextType
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
