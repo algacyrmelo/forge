92dfa2e3fce5195c9f520f364e6ed7eb2b7f1a8b
Algacyr Junior <alsdmelo@protonmail.com>
2026-03-16 12:54:48 -0300

    Add tests for text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -36,6 +36,55 @@ class TestTextNode(unittest.TestCase):
         )
 
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_italic(self):
+        node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic")
+
+    def test_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+
+    def test_link(self):
+        node = TextNode(
+            "Go to dashboard",
+            TextType.LINK,
+            "https://www.boot.dev/dashboard",
+        )
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Go to dashboard")
+        self.assertEqual(
+            html_node.props,
+            {"href": "https://www.boot.dev/dashboard"},
+        )
+
+    def test_image(self):
+        node = TextNode("boots icon", TextType.IMAGE, "images/boots-icon.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"alt": "boots icon", "src": "images/boots-icon.png"},
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
-
