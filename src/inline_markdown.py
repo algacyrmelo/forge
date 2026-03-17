c484768efaeaaa4689cc6366aab7075e821d6cc5
Algacyr Junior <alsdmelo@protonmail.com>
2026-03-17 19:31:05 -0300

    Implement extract_markdown_images/links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
 
 
@@ -7,15 +9,31 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
-        parts = old_node.text.split(delimiter)
-        if len(parts) % 2 == 0:
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
             raise Exception("invalid markdown, formatted section not closed")
-        for i in range(len(parts)):
-            if parts[i] == "":
+        for i in range(len(sections)):
+            if sections[i] == "":
                 continue
             if i % 2 == 0:
-                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
                 continue
             else:
-                new_nodes.append(TextNode(parts[i], text_type))
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(
+        r"!\[([^\[\]]*)\]\(([^\(\)]*)\)",
+        text,
+    )
+
+
+def extract_markdown_links(text):
+    return re.findall(
+        r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",
+        text,
+    )
