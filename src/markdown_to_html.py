e4550037538c8b09163e8295e6ea26d2313c489c
Algacyr Junior <alsdmelo@protonmail.com>
2026-03-24 17:50:42 -0300

    Add more handlers to markdown_to_html

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,26 +1,46 @@
 from htmlnode import ParentNode
 from inline_markdown import text_to_textnodes
 from markdown_blocks import BlockType, block_to_block_type, markdown_to_blocks
-from textnode import text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
-    root_children = []
+    children = []
     for block in blocks:
         block_type = block_to_block_type(block)
         match block_type:
             case BlockType.PARAGRAPH:
-                paragraph = block_to_paragraph(block)
-                root_children.append(paragraph)
+                htmlnode = block_to_paragraph(block)
+                children.append(htmlnode)
+            case BlockType.HEADING:
+                htmlnode = block_to_heading(block)
+                children.append(htmlnode)
+            case BlockType.CODE:
+                htmlnode = block_to_code(block)
+                children.append(htmlnode)
             case _:
                 pass
-    return ParentNode("div", root_children)
+    return ParentNode("div", children)
+
+
+def block_to_code(block):
+    text = block.lstrip("`\n").rstrip("`")
+    textnode = TextNode(text, TextType.CODE)
+    child = text_node_to_html_node(textnode)
+    return ParentNode("pre", [child])
+
+
+def block_to_heading(block):
+    text = block.lstrip("#")
+    heading_level = len(block) - len(text)
+    block_children = text_to_children(text.strip())
+    return ParentNode(f"h{heading_level}", block_children)
 
 
 def block_to_paragraph(block):
-    block = block.replace("\n", " ")
-    block_children = text_to_children(block)
+    text = block.replace("\n", " ")
+    block_children = text_to_children(text)
     return ParentNode("p", block_children)
 
 
